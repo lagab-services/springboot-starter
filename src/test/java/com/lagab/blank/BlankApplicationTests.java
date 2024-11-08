e9091ba1adfffc5cbf5105a101dd45bda763d8c7
Ladislas GABRIEL <gabriel.ladislas@kiabi.com>
2024-11-08 12:41:54 +0100

    init test

--- src/test/java/com/lagab/blank/BlankApplicationTests.java
+++ src/test/java/com/lagab/blank/BlankApplicationTests.java
@@ -1,13 +1,19 @@
 package com.lagab.blank;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Tag("context")
 @SpringBootTest
+@DisplayName("Context test for MainApplication")
 class BlankApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    @DisplayName("Context loads successfully")
+    void contextLoads() {
+        BlankApplication.main(new String[0]);
+    }
 
 }
